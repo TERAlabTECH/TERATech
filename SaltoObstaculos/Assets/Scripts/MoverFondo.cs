14934534603e0acb51f1d630f107530703cda957
MarielGuZa <marielsgtzz@gmail.com>
2023-03-22 22:49:57 -0600

    Mayor velocidad escena

--- SaltoObstaculos/Assets/Scripts/MoverFondo.cs
+++ SaltoObstaculos/Assets/Scripts/MoverFondo.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoverFondo : MonoBehaviour
 {
-    private float vel = 0.3f;
+    private float vel = 0.5f;
     private Renderer rend;
     // Start is called before the first frame update
     void Start()
