2f3c5b80dbefbb65b6c9f7e66d8365812f44bc4e
MarielGuZa <marielsgtzz@gmail.com>
2023-03-22 21:53:07 -0600

    Personaje corriendo

--- SaltoObstaculos/Assets/Scripts/MoverFondo.cs
+++ SaltoObstaculos/Assets/Scripts/MoverFondo.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoverFondo : MonoBehaviour
 {
-    private float vel = 0.2f;
+    private float vel = 0.3f;
     private Renderer rend;
     // Start is called before the first frame update
     void Start()
