9e3edd092b6005fa9c4c8d63276fca5333fe97df
Marielsgtzz <marielsgtzz@gmail.com>
2023-10-13 15:34:36 -0600

    Movimiento circular restringido al suelo

--- PatronesAnimales/Assets/Anterior/Scripts/DinosaurioObject.cs
+++ PatronesAnimales/Assets/Anterior/Scripts/DinosaurioObject.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class DinosaurioObject : MonoBehaviour
 {
-    private GameObject _dinosaurioObject;
+    private GameObject _personajeObject;
 
     public void SetObject(GameObject newObject) {
-        //Destruyo el objeto anterior
-        Destroy(_dinosaurioObject);
 
         //Agrego el nuevo objeto
-        _dinosaurioObject = Instantiate(newObject, this.transform); //Coloca el obj nuevo como hijo
+        _personajeObject = Instantiate(newObject, this.transform); //Coloca el obj nuevo como hijo
     }
 }
--- PatronesAnimales/Assets/Scripts/GameController.cs
+++ PatronesAnimales/Assets/Scripts/GameController.cs
@@ -6,45 +6,95 @@ using System;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private GameObject personaje;
+    [Header("Personaje Tipo 1")] //Los diferentes objetos que están en la escena que quiero asociar para darles funcionamiento/obtener info
+    [SerializeField] private GameObject personaje1Tipo1;
+    [SerializeField] private GameObject personaje2Tipo1;
+    [SerializeField] private GameObject personaje3Tipo1;
+
+    [SerializeField] private GameObject personajeTipo2;
+    [SerializeField] private GameObject personajeTipo3;
     [SerializeField] private GameObject suelo;
-    private Personaje p;
-    private Vector3 originalPos;
-    private Vector3 posSuelo;
-    private Vector3 posPersonaje;
-    private float radioCirculoZ, radioCirculoX;
 
+    //Variables internas a la clase
+    private Personaje[] pT1;
+    private Vector3[] pT1initialOffset;
 
-    private void Start()
-    { 
-        p = personaje.gameObject.GetComponent<Personaje>();
-        originalPos = new Vector3(p.gameObject.transform.position.x, p.gameObject.transform.position.y, p.gameObject.transform.position.z);
-        Debug.Log(originalPos);
-        Debug.Log("Suelo" + posSuelo);
-    }
+    private Personaje p1T1;
+    private Personaje p2T1;
+    private Personaje p3T1;
 
-    //void Update()
-    //{
-    //    Debug.Log(p.gameObject.transform.position);
+    private Personaje p1T2;
+    private Personaje p2T2;
+    private Personaje p3T2;
 
-    //    if (p.transform.position.z >= -0.216f && p.transform.position.z < 3.27f)
-    //    {
-    //        p.transform.Translate(0, 0, Time.deltaTime * 0.05f); //deltaTime es el tiempo entre un frame y otro
-    //    } else if (p.transform.position.z >= 3.27f) {
-    //        p.transform.Translate(0, 0, Time.deltaTime * -0.05f);
-    //    }
-    //}
+    private Personaje p1T3;
+    private Personaje p2T3;
+    private Personaje p3T3;
 
     float timeCounter = 0;
 
-    void Update()
+    private void Start()
     {
-        radioCirculoZ = suelo.gameObject.transform.position.z / 2;
-        timeCounter += Time.deltaTime;
-        float x = suelo.gameObject.transform.position.x + radioCirculoZ * Mathf.Cos(timeCounter);
-        float z = suelo.gameObject.transform.position.z + radioCirculoZ * Mathf.Sin(timeCounter);
-        Debug.Log(originalPos);
-        p.transform.position = new Vector3(x, suelo.gameObject.transform.position.y + 0.04f, z);
+        p1T1 = personaje1Tipo1.gameObject.GetComponent<Personaje>();
+        p2T1 = personaje2Tipo1.gameObject.GetComponent<Personaje>();
+        p3T1 = personaje3Tipo1.gameObject.GetComponent<Personaje>();
+
+        pT1 = new Personaje[] { p1T1,p2T1,p3T1};
+        pT1initialOffset = new Vector3[] {
+            p1T1.gameObject.transform.position - suelo.transform.position,
+            p2T1.gameObject.transform.position - suelo.transform.position,
+            p3T1.gameObject.transform.position - suelo.transform.position
+        }; 
+
+
+        //originalPos = new Vector3(p1T1.gameObject.transform.position.x, p1T1.gameObject.transform.position.y, p1T1.gameObject.transform.position.z);
+        //Debug.Log(originalPos);
+        //Debug.Log("Suelo" + posSuelo);
     }
 
+    //Se llama en cada frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime;
+
+        moverPersonajeTipo1();
+
+    }
+
+    //Mueve los objetos en una trayectoria circular
+    public void moverPersonajeTipo1() {
+        float x, z;
+        float radioCirculo;
+        for (int i = 0; i < 3; i++) {
+            radioCirculo = restrictCircularMovement(pT1initialOffset[i]);
+            x = suelo.gameObject.transform.position.x + pT1initialOffset[i].x - radioCirculo * Mathf.Cos(timeCounter);
+            z = suelo.gameObject.transform.position.z + pT1initialOffset[i].z - radioCirculo * Mathf.Sin(timeCounter);
+
+            switch (i) {
+                case 0:
+                    p1T1.transform.position = new Vector3(x, suelo.gameObject.transform.position.y + 0.04f, z);
+                    break;
+                case 1:
+                    p2T1.transform.position = new Vector3(x, suelo.gameObject.transform.position.y + 0.04f, z);
+                    break;
+                case 2:
+                    p3T1.transform.position = new Vector3(x, suelo.gameObject.transform.position.y + 0.04f, z);
+                    break;
+            }
+        }
+    }
+
+    public float restrictCircularMovement(Vector3 initialOffset) {
+        float res;
+        float quadrantSize = suelo.gameObject.transform.localScale.z / 2;//Supone que el suelo es un cuadrado no rectangulo
+        float x = Math.Abs(initialOffset.x);
+        float z = Math.Abs(initialOffset.z);
+
+        float distanceToEdgeX = quadrantSize - x;
+        float distanceToEdgeZ = quadrantSize - z;
+
+        res = Mathf.Min(distanceToEdgeX, distanceToEdgeZ);
+
+        return res;
+    }
 }
