fac777c82aee641855b59498e6f082698e42beba
Marielsgtzz <marielsgtzz@gmail.com>
2023-10-13 16:34:52 -0600

    Comments and cleanup

--- PatronesAnimales/Assets/Scripts/GameController.cs
+++ PatronesAnimales/Assets/Scripts/GameController.cs
@@ -8,35 +8,43 @@ using System;
 
 public class GameController : MonoBehaviour
 {
-    [Header("Personaje Tipo 1")] //Los diferentes objetos que están en la escena que quiero asociar para darles funcionamiento/obtener info
+    //Los diferentes objetos que están en la escena que quiero asociar para darles funcionamiento/obtener info
+    [Header("Personaje Tipo 1")] 
     [SerializeField] private GameObject personaje1Tipo1;
     [SerializeField] private GameObject personaje2Tipo1;
     [SerializeField] private GameObject personaje3Tipo1;
 
-    [SerializeField] private GameObject personajeTipo2;
-    [SerializeField] private GameObject personajeTipo3;
-    [SerializeField] private GameObject suelo;
+    // Aquí va a ser similar pero con los otros modelos
 
-    //Variables internas a la clase
-    private Personaje[] pT1;
-    private Vector3[] pT1initialOffset;
+    //Agregamos un suelo a la experiencia para que no se pierdan los modelos con el suelo real
+    [SerializeField] private GameObject suelo; //Debería de ser cuadrado, no rectangular
 
+    //Personajes de tipo 1 (son del mismo modelo) y se mueven en trayectoria circular
     private Personaje p1T1;
     private Personaje p2T1;
     private Personaje p3T1;
 
-    private float[] pT1Radio;
-    private float[] pT1Speed;
+    //Variables internas a la clase
+    private Personaje[] pT1; //Arreglo de los personajes de tipo 1 
+    private Vector3[] pT1initialOffset; //Arreglo para guardar las posiciones iniciales de los modelos tipo 1
+
+    
+    private float[] pT1Radio; //Arreglo de los radios para los movimientos para los personajes tipo 1
+    private float[] pT1Speed; //Arreglo de las velocidades para los movimientos de los personajes tipo 1
 
     float timeCounter = 0;
 
+    //Se llama al inicio (1 vez) cuando se carga el juego
     private void Start()
     {
+        //Toma los game objects dados y los guarda como personajes
+        //Ahoritan son esferas pero van a ser modelos .fbx
         p1T1 = personaje1Tipo1.gameObject.GetComponent<Personaje>();
         p2T1 = personaje2Tipo1.gameObject.GetComponent<Personaje>();
         p3T1 = personaje3Tipo1.gameObject.GetComponent<Personaje>();
 
         pT1 = new Personaje[] { p1T1,p2T1,p3T1};
+
         pT1initialOffset = new Vector3[] {
             p1T1.gameObject.transform.position - suelo.transform.position,
             p2T1.gameObject.transform.position - suelo.transform.position,
@@ -44,11 +52,13 @@ public class GameController : MonoBehaviour
         };
 
         pT1Radio = new float[3];
+        //Para que la trayectoria circular de cada personaje sea diferente entre partidas
         for (int i = 0; i < 3; i++)
         {
             pT1Radio[i] = restrictCircularMovement(pT1initialOffset[i]);
         }
 
+        //Para que la velocidad de cada personaje sea diferente entre partidas
         pT1Speed = new float[] {
             UnityEngine.Random.Range(0.8f, 6),
             UnityEngine.Random.Range(0.8f, 6),
@@ -88,14 +98,12 @@ public class GameController : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="initialOffset"></param>
-    /// <returns></returns>
+    //Toma la posición del personaje y se asegura que el radio sea menor al espacio que queda
+    //entre el personaje y el borde del suelo.
+    //Esto con la intencion de que la trayectoria circular no ocurra fuera del suelo.
     public float restrictCircularMovement(Vector3 initialOffset) {
         float res;
-        float quadrantSize = suelo.gameObject.transform.localScale.z / 2;//Supone que el suelo es un cuadrado no rectangulo
+        float quadrantSize = suelo.gameObject.transform.localScale.z / 2; //Supone que el suelo es un cuadrado no rectangulo
         float x = Math.Abs(initialOffset.x);
         float z = Math.Abs(initialOffset.z);
 
@@ -106,11 +114,4 @@ public class GameController : MonoBehaviour
 
         return UnityEngine.Random.Range(0.05f, res); 
     }
-
-    public float differentSpeeds() {
-        float res = 0.0f;
-
-        return res;
-    }
-
 }
