39be9fa892ec7371d0d6f0f8a13d4ccd5d5afaed
TERA tech <tera@itam.mx>
2024-09-27 13:40:26 -0600

    Creacion escena galeria
    solo pusimos el fondo, aun no tiene funcionalidad

--- DiveInn/Assets/Scripts/Galeria/GaleriaManager.cs
+++ DiveInn/Assets/Scripts/Galeria/GaleriaManager.cs
@@ -1,22 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class GaleriaManager : MonoBehaviour
 {
-    Transform trans; 
-
     // Start is called before the first frame update
     void Start()
     {
-        trans= gameObject.GetComponent<Transform>(); 
-        
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        trans.position+=new Vector3(-0.01f,0,0);
+        Debug.Log("PErroooo");
     }
 }
